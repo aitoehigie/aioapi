005c378316b75d449430bf326ad2da8830a5f51a
aitoehigie <aitoehigie@gmail.com>
2017-10-29 20:09:30 +0100

    create a user function in handler module

--- app.py
+++ app.py
@@ -7,7 +7,18 @@ async def handle(request):
     response_obj = {"status": "success"}
     return web.Response(text=json.dumps(response_obj))
 
+async def new_user(request):
+    try:
+        user = request.query["name"]
+        print("Creating new user with name: ", user)
+        response_obj - {"status": "success"}
+        return web.Response(text=json.dumps(response_obj), status=200)
+    except Exception as e:
+        response_obj = {"status": "failed", "reason": str(e)}
+        return web.Response(text=json.dumps(response_obj), status=500)
+
 app = web.Application()
 app.router.add_get("/", handle)
+app.router.add_get("/user", new_user)
 
 web.run_app(app)
